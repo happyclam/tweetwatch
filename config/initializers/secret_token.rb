d567843fa3eef73ee80493930530ffd41e423d5b
happyclam <happyclam.u@gmail.com>
2014-02-22 03:21:43 +0000

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tweetwatch::Application.config.secret_key_base = 'eb302d99d9ba05d92fd146fd276aff78650d07c1014cf174bd93a02388407fef703baa579977555da5567d83be7392605b36719fdfc96c7213a30a4cde26d4ab'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tweetwatch::Application.config.session_store :cookie_store, key: '_tweetwatch_session'
