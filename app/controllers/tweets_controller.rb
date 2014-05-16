53f541356a5c8f961c65954abcd4fb09af82cc66
happyclam <happyclam.u@gmail.com>
2014-05-16 21:20:46 +0900

    ・遅いPCだとsleepの値を増やさないとダメみたい、１から５に ・twitter用のaouth認証に使う値を流し込むquery作成して.gitignoreに ・Usersテーブルにtwitter認証キーを格納するカラム４つを追加 ・サーバースクリプト起動時にtwitter認証キーを起動パラメータに追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -34,15 +34,21 @@ p "status_check="+@status_check.to_s
 p "tweets.start"
     user_id = params["user"]
     track = current_user.serv.track
+    c = current_user.c_key
+    k = current_user.c_secret
+    s = current_user.a_key
+    a = current_user.a_secret
     unless track
       flash[:alert] = "右側のリストから、タグを指定してください"
       render :js => "window.location.href='"+user_path(user_id)+"'"
       return
     end
     begin
-      ret = system("ruby myserv.rb -p#{10000 + user_id.to_i} -t#{track} &")
+p "ruby myserv.rb -p#{10000 + user_id.to_i} -t#{track} -c#{c} -k#{k} -s#{s} -a#{a} &"
+      ret = system("ruby myserv.rb -p#{10000 + user_id.to_i} -t#{track} -c#{c} -k#{k} -s#{s} -a#{a} &")
+
       current_user.serv.start if ret
-      ret = system("sleep 1")
+      ret = system("sleep 5")
     rescue
       current_user.serv.down
       render :js => "window.location.href='"+user_path(user_id)+"'"
@@ -55,13 +61,12 @@ p "tweets.start"
 
   def store
 p "tweets.store"
-    user_id = current_user.id
 p "user_id="+current_user.id.to_s
-p "store:track="+current_user.serv.track.to_s
+p "store:track="+current_user.serv.track
     begin
       localhost = Net::Telnet::new("Host" => "localhost",
-                                   "Port" => 10000 + user_id.to_i,
-                                   "Timeout" => 5,
+                                   "Port" => 10000 + current_user.id,
+                                   "Timeout" => 1,
                                    "Telnetmode" => false,
                                    "Output_log" => "./output.log",
                                    "Dump_log" => "./dump.log",
@@ -73,12 +78,14 @@ p "store:track="+current_user.serv.track.to_s
       current_user.serv.store if current_user.serv.status == PREPARED      
     rescue
       @status_store = false
-#      current_user.serv.track = nil
+      current_user.serv.stop
+      flash[:alert] = $!.to_s
       p $!
+      return redirect_to user_path(current_user), notice: $!.to_s
     end
 p "status_store="+@status_store.to_s
 p "server_status="+current_user.serv.status.to_s
-    return redirect_to user_path(user_id)
+    redirect_to user_path(current_user.id)
 
   end
 
@@ -99,12 +106,12 @@ p "status="+current_user.serv.status.to_s
       localhost.close
       localhost = nil
       @status_stop = true
-      current_user.serv.stop if current_user.serv
     rescue
       @status_stop = false
       flash[:alert] = $!.to_s
       p $!
     end
+    current_user.serv.stop if current_user.serv
 p "status="+current_user.serv.status.to_s
     render
 
